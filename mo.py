360c9c8508e8ebf0fd71136ac4e42f6c46f07506
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-25 20:09:18 +0330

    Add first argument, --help (-h)

---
+++ mo.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="MO, the Musicophile Owl")
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == "__main__":
+    main()
+
