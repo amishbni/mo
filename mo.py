96103c9ea0a8b1e7e93196ac5af44a86503f9808
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-25 20:29:35 +0330

    Add --play (-p) argument

--- mo.py
+++ mo.py
@@ -2,6 +2,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="MO, the Musicophile Owl")
+    parser.add_argument("-p", "--play", nargs=1, help="play an audio file")
     args = parser.parse_args()
     print(args)
 
