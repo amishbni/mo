e2143adb75f68eeb804e48fd496a9fc58c5c8d43
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-25 21:48:27 +0330

    Change --play to --info and print audio's information in the format {Artist - Title}

--- mo.py
+++ mo.py
@@ -1,10 +1,17 @@
 import argparse
+from mutagen.easyid3 import EasyID3
+
+def metadata(filepath):
+    audio = EasyID3(filepath)
+    print(f"{audio['artist'][0]} - {audio['title'][0]}")
 
 def main():
     parser = argparse.ArgumentParser(description="MO, the Musicophile Owl", prog="mo")
-    parser.add_argument("-p", "--play", nargs=1, help="play an audio file")
-    args = parser.parse_args()
-    print(args)
+    parser.add_argument("--info", nargs=1, help="show information of an audio file")
+    args = vars(parser.parse_args())
+
+    if(args["info"]):
+        metadata(args["info"][0])
 
 if __name__ == "__main__":
     main()
