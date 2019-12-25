154451ca99249e411ea89127ff512d822ec8c304
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-25 20:34:18 +0330

    Change program name from sys.argv[0] to mo

--- mo.py
+++ mo.py
@@ -1,7 +1,7 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="MO, the Musicophile Owl")
+    parser = argparse.ArgumentParser(description="MO, the Musicophile Owl", prog="mo")
     parser.add_argument("-p", "--play", nargs=1, help="play an audio file")
     args = parser.parse_args()
     print(args)
